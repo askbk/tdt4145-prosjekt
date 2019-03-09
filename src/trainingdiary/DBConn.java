ff438e21c721acf7fccd5fa3d0a698a2e6e0f263
ask <askbk@protonmail.com>
2019-03-09 12:46:32 +0100

    Abstrakt klasse for å koble til database

---
+++ src/trainingdiary/DBConn.java
@@ -0,0 +1,23 @@
+package trainingdiary;
+
+import java.sql.*;
+import  java.util.Properties;
+
+public abstract class DBConn {
+	protected Connection conn;
+	public DBConn() {}
+	
+	public void connect() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			Properties p = new Properties();
+			
+			p.put("user", "myuser");
+			p.put("password", "mypassword");
+			
+			conn = DriverManager.getConnection("jdb:mysql://127.0.0.1/")
+		} catch (Exception e) {
+			throw new RuntimeException("Unable to connect", e);
+		}
+	}
+}
