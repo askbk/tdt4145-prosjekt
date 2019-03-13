708945843da8ef688b92c53994b3313095ea3fef
ask <askbk@protonmail.com>
2019-03-13 18:35:21 +0100

    Begynte på brukerinteraksjon

--- src/trainingdiary/Main.java
+++ src/trainingdiary/Main.java
@@ -1,9 +1,27 @@
 package trainingdiary;
 
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        LagApparatCtrl lagApparatCtrl = new LagApparatCtrl();
-        lagApparatCtrl.lagApparat("Romaskin", "Man ror på denne");
-        lagApparatCtrl.fullførApparat();
+        // LagApparatCtrl lagApparatCtrl = new LagApparatCtrl();
+        //  lagApparatCtrl.lagApparat("Romaskin", "Man ror på denne");
+        //  lagApparatCtrl.fullførApparat();
+        System.out.println("Hello");
+        Console cons = System.console();
+        String action = cons.readLine(); 
+        switch(action) {
+        	case "0":
+        		System.out.println("valg 0");
+        		break;
+        	case "1":
+        		System.out.println("valg 1");
+        		break;
+        	default:
+        		System.out.println("velg 1 eller 0");
+        		break;
+        }
     }
+    
+    
 }
