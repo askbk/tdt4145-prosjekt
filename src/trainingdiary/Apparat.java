327842a665479ce11f6104988b0daad98adec5dc
ask <askbk@protonmail.com>
2019-03-13 15:31:06 +0100

    La fylte ut initialize-metode

--- src/trainingdiary/Apparat.java
+++ src/trainingdiary/Apparat.java
@@ -1,6 +1,10 @@
 package trainingdiary;
 
-public class Apparat {
+import java.sql.*;
+import java.util.*;
+
+public class Apparat extends ActiveDomainObject {
+	private int apparadId = -1;
 	private String navn;
 	private String beskrivelse;
 	
@@ -15,5 +19,33 @@ public class Apparat {
 	
 	public String getBeskrivelse() {
 		return this.beskrivelse;
+	}
+
+	@Override
+	public void initialize(Connection conn) {
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select navn, beskrivelse from Apparat where ApparatID=" + apparatId);
+            while (rs.next()) {
+                this.navn =  rs.getString("navn");
+                this.beskrivelse = rs.getString("beskrivelse");
+            }
+
+        } catch (Exception e) {
+            System.out.println("db error during select of apparat= "+e);
+            return;
+        }
+	}
+
+	@Override
+	public void refresh(Connection conn) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void save(Connection conn) {
+		// TODO Auto-generated method stub
+		
 	}
 }
