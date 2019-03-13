607485678014caa1039337d31f27c7084e1070d4
Erlend Sæther <erlend.sae@hotmail.com>
2019-03-13 15:44:19 +0100

    added refresh,save and initialize

--- src/trainingdiary/Notat.java
+++ src/trainingdiary/Notat.java
@@ -1,6 +1,11 @@
 package trainingdiary;
 
-public class Notat {
+import java.sql.*;
+import java.util.*;
+
+
+public class Notat extends ActiveDomainObject{
+	private int notatID
 	private String treningsFormaal;
 	private String treningsOpplevelse;
 	
@@ -15,6 +20,40 @@ public class Notat {
 
 	public String getTreningsOpplevelse() {
 		return this.treningsOpplevelse;
+	}
+
+	@Override
+	public void initialize(Connection conn) {
+		try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select treningsformaal, treningsOpplevelse from Notat where notatID=" + notatID);
+            while (rs.next()) {
+                treningsFormaal =  rs.getString("treningsformaal");
+                treningsOpplevelse = rs.getString("treningsopplevelse");
+              }
+
+        } catch (Exception e) {
+            System.out.println("db error during select of avtale= "+e);
+            return;
+        }
+		
+	}
+
+	@Override
+	public void refresh(Connection conn) {
+		initialize (conn);	
+	}
+
+	@Override
+	public void save(Connection conn) {
+		try {    
+            Statement stmt = conn.createStatement(); 
+            stmt.executeUpdate("insert into Notat(treningsFormaal,treningsOpplevelse) values (" + this.treningsFormaal + "," + this.treningsOpplevelse + ")");
+        } catch (Exception e) {
+            System.out.println("db error during insert of Notat="+e);
+            return;
+        }
+		
 	}
 
 	
