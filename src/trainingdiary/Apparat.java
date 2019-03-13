9d1b16a7ebe35e2240bb97893a3fcf2a3c097515
ask <askbk@protonmail.com>
2019-03-13 15:08:02 +0100

    La attributter til apparat

--- src/trainingdiary/Apparat.java
+++ src/trainingdiary/Apparat.java
@@ -1,7 +1,19 @@
 package trainingdiary;
 
 public class Apparat {
-	public Apparat() {
-		
+	private String navn;
+	private String beskrivelse;
+	
+	public Apparat(String navn, String beskrivelse) {
+		this.navn = navn;
+		this.beskrivelse = beskrivelse;
+	}
+	
+	public String getNavn() {
+		return this.navn;
+	}
+	
+	public String getBeskrivelse() {
+		return this.beskrivelse;
 	}
 }
