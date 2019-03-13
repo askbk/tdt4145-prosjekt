d79d3762f179a216abd46b8cacf57d29f7f91a31
Erlend Sæther <erlend.sae@hotmail.com>
2019-03-13 15:19:53 +0100

    added Notat class

---
+++ src/trainingdiary/Notat.java
@@ -0,0 +1,23 @@
+package trainingdiary;
+
+public class Notat {
+	private String treningsFormaal;
+	private String treningsOpplevelse;
+	
+	public Notat(String treningsFormaal, String treningsOpplevelse) {
+		this.treningsFormaal = treningsFormaal;
+		this.treningsOpplevelse = treningsOpplevelse;				
+	}
+
+	public String getTreningsFormaal() {
+		return this.treningsFormaal;
+	}
+
+	public String getTreningsOpplevelse() {
+		return this.treningsOpplevelse;
+	}
+
+	
+	
+	
+}
