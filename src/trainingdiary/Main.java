944a2162411bebbf0eca07676db7e97c4d5d754c
ask <askbk@protonmail.com>
2019-03-15 11:11:24 +0100

    Fjernet ø

--- src/trainingdiary/Main.java
+++ src/trainingdiary/Main.java
@@ -1,26 +1,24 @@
 package trainingdiary;
 
 import java.io.*;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // LagApparatCtrl lagApparatCtrl = new LagApparatCtrl();
-        //  lagApparatCtrl.lagApparat("Romaskin", "Man ror på denne");
-        //  lagApparatCtrl.fullførApparat();
-        System.out.println("Hello");
-        Console cons = System.console();
-        String action = cons.readLine(); 
-        switch(action) {
-        	case "0":
-        		System.out.println("valg 0");
-        		break;
-        	case "1":
-        		System.out.println("valg 1");
-        		break;
-        	default:
-        		System.out.println("velg 1 eller 0");
-        		break;
-        }
+        LagApparatCtrl lagApparatCtrl = new LagApparatCtrl();
+        lagApparatCtrl.lagApparat("Romaskin", "Man ror på denne");
+        lagApparatCtrl.fullforApparat();
+        
+        //switch(action) {
+       // 	case "0":
+       // 		System.out.println("valg 0");
+       // 		break;
+       // 	case "1":
+       // 		System.out.println("valg 1");
+       // 		break;
+       // 	default:
+       // 		System.out.println("velg 1 eller 0");
+       // 		break;
+        //}
     }
     
     
