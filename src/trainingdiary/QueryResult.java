ce7f6b7ca32a6c0406f6259e182a8c4a303d63b9
ask <askbk@protonmail.com>
2019-03-20 14:07:05 +0100

    Implementerte klasse for å lagre resultat fra queries

---
+++ src/trainingdiary/QueryResult.java
@@ -0,0 +1,33 @@
+package trainingdiary;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+public class QueryResult implements Iterable<List<String>>{
+	private List<Integer> keys;
+	private HashMap<Integer, List<String>> rows;
+	
+	public QueryResult() {
+		keys = new ArrayList<>();
+		rows = new HashMap<>();
+	}
+	
+	public void insertRow(int key, List<String> cols) {
+		keys.add(key);
+		rows.put(key, cols);
+	}
+	
+	public List<String> getResult(int key) {
+		return this.rows.get(key);
+	}
+
+	@Override
+	public Iterator<List<String>> iterator() {
+		Iterator<List<String>> ires = rows.values().iterator();
+        return ires; 
+	}
+	
+	
+}
