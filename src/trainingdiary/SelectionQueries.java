6178b969d5ca4d26ddfc6188390d1a4e15daf23a
Alexander Bollestad <alexander.bollestad@gmail.com>
2019-03-21 02:26:37 +0100

    La til ikkefungerende Query for Resultatlogg
    Tror vi trenger å legge til noe ekstra for at denne skal funke. Men kanskje også ikke.

--- src/trainingdiary/SelectionQueries.java
+++ src/trainingdiary/SelectionQueries.java
@@ -78,6 +78,41 @@ public class SelectionQueries extends DBConn{
 		return result;
 	}
 	
+	public QueryResult getResultatlogg(int dato1, int dato2) {
+		connect();
+		int key;
+		QueryResult result = new QueryResult();
+		String treningsFormaal;
+		String treningsOpplevelse;
+		List<String> row;
+		
+		try {
+			System.out.println("dheya");
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("select*"
+											+ "from Okt as o"
+											+ "left join Notat as n on o.OktId = n.OktId"
+											+ "order by o.OktId desc limit " + k);
+		while (rs.next()) {
+			row = new ArrayList<>();
+			
+			key = rs.getInt("oktId");
+			treningsFormaal = rs.getString("treningsFormaal");
+			treningsOpplevelse = rs.getString("treningsOpplevelse");
+			row.add(Integer.toString(key));
+			row.add(treningsFormaal);
+			row.add(treningsOpplevelse);
+			
+			result.insertRow(key, row);
+		}
+		} catch (Exception e) {
+			System.out.println("db error during select of okt= "+e);
+		}
+		
+		return result;
+	}
+	
+	
 	public static void main(String[] args) {
 		SelectionQueries test = new SelectionQueries();
 		QueryResult apparater = test.getApparater();
